cef6a92ccf187331fefc3a66d87d9c9f5ec33219
minsgy <alstjrchl97@naver.com>
2020-09-01 20:48:53 +0900

    #11.5 paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,15 @@
-from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
+from django.core.paginator import Paginator
 from . import models
 
 
 # Create your views here.
 def home(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", {"all_rooms": all_rooms})
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 5, orphans=2)  # 5개씩 끊기
+    rooms = paginator.get_page(page)
+    return render(request, "rooms/home.html", {"page": rooms})
+
