bb6133ee59805354d5cee36cd7aa9cdc11b48df8
minsgy <alstjrchl97@naver.com>
2020-09-13 15:55:54 +0900

    #12.4 CBV의 사기성

--- rooms/views.py
+++ rooms/views.py
@@ -13,9 +13,8 @@ class HomeView(ListView):
     context_object_name = "rooms"  # 객체를 부르는 이름을 변경
 
 
-class RoomDetail(DetailView):  # detail 값 다 반환
+class RoomDetail(DetailView):  # 미친 개쉽다;; 리스트값 전부 반환..
     model = models.Room
-
 
 
 #  page_kwarg = "potato"  # page 이름
