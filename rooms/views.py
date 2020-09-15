83d589a51f574b33f3ef48841e81f2366671121a
minsgy <alstjrchl97@naver.com>
2020-09-15 15:20:04 +0900

    #13.1 Form

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.http import Http404
+from django_countries import countries
 from . import models
 
 
@@ -18,9 +19,14 @@ class RoomDetail(DetailView):  # 미친 개쉽다;; 리스트값 전부 반환..
 
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_type": room_types},
+    )
 
 
 #  page_kwarg = "potato"  # page 이름
