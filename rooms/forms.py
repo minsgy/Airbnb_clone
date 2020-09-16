17ba4d954b7ba69d374053cb062976eb02359bd2
minsgy <alstjrchl97@naver.com>
2020-09-16 11:40:07 +0900

    #13.8 Django Forms Used

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,8 +1,29 @@
 from django import forms
+from django_countries.fields import CountryField
 from . import models
 
 
 class SearchForm(forms.Form):
     city = forms.CharField(initial="Anywhere")  # 초기 값 지정
+    country = CountryField(
+        default="KR"
+    ).formfield()  # django country에서 자체적으로 지원하는 Forms
     price = forms.IntegerField(required=False)
-    room_type = forms.ModelChoiceField(queryset=models.RoomType.objects.all())
+    room_type = forms.ModelChoiceField(
+        empty_label="Any kind", queryset=models.RoomType.objects.all()
+    )
+    price = forms.IntegerField(required=False)
+    guests = forms.IntegerField(required=False)
+    bedrooms = forms.IntegerField(required=False)
+    beds = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    instant_book = forms.BooleanField(required=False)
+    superhost = forms.BooleanField(required=False)
+
+    amenities = forms.ModelMultipleChoiceField(
+        queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple()
+    )
+    facilities = forms.ModelMultipleChoiceField(
+        queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple()
+    )
+
