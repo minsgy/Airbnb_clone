278291ff70fb8bbb3359e4682afa89307547ba84
minsgy <alstjrchl97@naver.com>
2020-09-20 14:52:07 +0900

    #14.3

--- users/forms.py
+++ users/forms.py
@@ -8,16 +8,18 @@ class LoginForm(forms.Form):
         widget=forms.PasswordInput
     )  # passwordinput 뿐.. 용도가 다르다 근데
 
-    def clean_email(self):
-        # 무엇을 검색할 때,method 이름은 무조건 clean_(필드이름) << 이여야함.
-        # 에러 출력용 및 데이터 정리
-        # return 안하면 값삭제됨.
+    def clean(self):  # 종속되어 있으므로 생략 가능
         email = self.cleaned_data.get("email")  # 사용자가 입력한 값 중 email을 골라 저장함.
+        password = self.cleaned_data.get("password")
         try:  # 실행 문구
-            models.User.objects.get(username=email)
-            return email
+            user = models.User.objects.get(email=email)  # 해당 이메일을 가진 유저를 user 저장
+            if user.check_password(password):
+                # user 모델의 정의된 check_password 메소드 사용
+                # 주어진 string이 맞으면 True, 아니면 False
+                return self.cleaned_data  # 다 맞다면, 입력한 email, password 데이터값 전달
+            else:
+                # 어디에 오류를 출력 할 지 정하기
+                self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:  # 실행 실패 시
-            raise forms.ValidationError("User does not exist")
-
-    def clean_password(self):
-        print("clean password")
+            # 어디에 오류를 출력 할 지 정하기
+            self.add_error("email", forms.ValidationError("User does not exist"))
--- users/views.py
+++ users/views.py
@@ -10,11 +10,7 @@ class LoginView(View):
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-<<<<<<< HEAD
-        if form.is_valid():  # clean_(data)은 리턴하지않으면 값이 삭제됌.
+        if form.is_valid():  # 유효한 값 인지 검사
 
             print(form.cleaned_data)
         return render(request, "users/login.html", {"form": form})
-=======
-        return render(request, "users/login.html",{"form": form})
->>>>>>> 323c4eca30edd8f678d9cf0242ea7e25e6e7a572
